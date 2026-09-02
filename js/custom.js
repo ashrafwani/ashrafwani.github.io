cb248048cdf4c5c489af6d3556c1978309583d74
Ashraf Wani <ashrafwani@outlook.com>
2026-09-02 11:45:17 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -154,16 +154,33 @@
         }
         if ($('.fullpage-default').length) {
             var myFullpage = new fullpage('.fullpage-default', {
-                licenseKey: ' C7F41B00-5E824594-9A5EFB99-B556A3D5',
-                anchors: ['slide01', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
+                licenseKey: 'C7F41B00-5E824594-9A5EFB99-B556A3D5',
+        
+                anchors: [
+                    'slide01',
+                    'slide02',
+                    'slide03',
+                    'slide04',
+                    'slide05',
+                    'slide06',
+                    'slide07'
+                ],
+        
                 menu: '#nav',
+        
                 lazyLoad: true,
+        
                 navigation: true,
                 navigationPosition: 'right',
+        
+                /* Desktop fullPage scrolling */
                 scrollOverflow: true,
-                responsiveWidth: 768,
+        
+                /* Switch to normal page scrolling on mobile */
+                responsiveWidth: 767,
                 responsiveHeight: 600,
-                responsiveSlides: true
+        
+                responsiveSlides: false
             });
         }
         $(document).on('click', '.navbar-toggle', function() {

8d0d951e4a8d06482d49fae5ad23b4bbe193ca57
Ashraf Wani <ashrafwani@outlook.com>
2026-09-22 12:50:12 +0530

    Fix license key formatting in custom.js
    Removed unnecessary whitespace in license key.

--- js/custom.js
+++ js/custom.js
@@ -154,13 +154,12 @@
         }
         if ($('.fullpage-default').length) {
             var myFullpage = new fullpage('.fullpage-default', {
-                licenseKey: ' C7F41B00-5E824594-9A5EFB99-B556A3D5',
+                licenseKey: 'C7F41B00-5E824594-9A5EFB99-B556A3D5',
                 anchors: ['slide01', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
                 menu: '#nav',
                 lazyLoad: true,
                 navigation: true,
                 navigationPosition: 'right',
-                scrollOverflow: true,
                 responsiveWidth: 768,
                 responsiveHeight: 600,
                 responsiveSlides: true
